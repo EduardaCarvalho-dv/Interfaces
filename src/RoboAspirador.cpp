ab397d3ff63e9b84b3c71e6bd2a4dc9aac9ab986
EduardaCarvalho-dv <eduardasantos132018@gmail.com>
2026-05-26 22:05:42 -0300

    add classes

---
+++ src/RoboAspirador.cpp
@@ -0,0 +1,1 @@
+#include "RoboAspirador.h"
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#include "LampadaInteligente.h"
+#include "CameraSeguranca.h"
+#include "FechaduraInteligente.h"
+#include "TermostatoDigital.h"
+#include "RoboAspirador.h"
+
+int main () {
+
+    return 0;
+};
